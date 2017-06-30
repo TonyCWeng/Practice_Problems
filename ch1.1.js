b187007b288ec1a407b1c46fcbb9b26abbc24f07
Tony Weng <tcweng91@gmail.com>
2017-06-30 14:21:22 -0700

    push -u origin master

---
+++ ch1.1.js
@@ -0,0 +1,23 @@
+/* Implement an algorithm to determine if a string has all unique characters.
+   2) What if you can not use additional data structures?
+*/
+
+const allUnique = str => {
+  const uniques = new Set();
+  for (let i = 0; i < str.length; i++) {
+    if (uniques.has(str[i])) return false;
+    uniques.add(str[i]);
+  }
+  return true;
+};
+
+const allUniqueAlt = str => {
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i+1; j < str.length; j++) {
+      if (str[i] === str[j]) return false;
+    }
+  }
+  return true;
+};
+
+console.log(allUniqueAlt("apple"));
