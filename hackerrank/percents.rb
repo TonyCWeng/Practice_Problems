33d1194e0d6896af76a5c1b810ec052daabcca24
Tony Weng <tcweng91@gmail.com>
2017-07-19 15:44:33 -0700

    solve plus_minus

---
+++ hackerrank/percents.rb
@@ -0,0 +1,17 @@
+def plus_minus(arr)
+  pos = 0
+  neg = 0
+  zeroes = 0
+
+  arr.each do |x|
+    case x <=> 0
+    when 1
+      post += 1
+    when 0
+      zeroes += 1
+    else
+      neg += 1
+    end
+  end
+  
+end
