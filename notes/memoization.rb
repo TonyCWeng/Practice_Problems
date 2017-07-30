0fbc5e4fc0f84329dabd1903109c56c717ce51c7
TonyCWeng <tcweng91@gmail.com>
2017-07-29 17:29:11 -0700

    add notes on memoization and tabulation

---
+++ notes/memoization.rb
@@ -0,0 +1,10 @@
+Memoization
+- Optimization technique through caching previously computed results.
+- Used when you wish to revisit computed values.
+- Differs from dynamic programming, which is a technique for solving problems
+recursively when the computations may overlap within subproblems?
+
+* One could say that memoization is a method of implementing dynamic programming.
+* The other method would be tabulation which is an iterative approach.
+- We split the problem into sub problems and solve those portions.
+- Main method calls on sub problems. 
