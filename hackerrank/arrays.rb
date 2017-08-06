401d7c54088ce4c71cb5ce018ffae50d952d2113
tonycweng <tcweng91@gmail.com>
2017-08-05 18:06:05 -0700

    complete simple array problem

---
+++ hackerrank/arrays.rb
@@ -0,0 +1,15 @@
+n = gets.strip.to_i
+arr = gets.strip
+arr = arr.split(' ').map(&:to_i)
+
+rev = arr.reverse
+len = arr.length
+i = 0
+while i < len
+  print rev[i]
+  print " "
+  i += 1
+end
+
+# they asked for something a bit weird, but I guess that they
+# did not check for an ending space?
--- misc.rb
+++ misc.rb
@@ -1,3 +1,3 @@
 def alsf
-
+  asdfasd
 end
