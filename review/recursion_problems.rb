cd35fa406efbbef64369dde9599d6d8f89685ed7
tonycweng <tcweng91@gmail.com>
2017-08-21 14:47:06 -0700

    solve num_occur, add_to_twelve, includes?

--- review/recursion_problems.rb
+++ review/recursion_problems.rb
@@ -2,24 +2,42 @@
 #sum of the integers.
 
 def sum_recur(array)
+  #base case
+  return 0 if array.empty?
+
+  #iterative step
+  array.first + sum_recur(array[1..-1])
 end
+
+# Sum_recur would have a linear time complexity, as we are iterating through
+# Each element of the input array.
+
 
 #Problem 2: You have array of integers. Write a recursive solution to determine
 #whether or not the array contains a specific value.
 
 def includes?(array, target)
+  return false if array.empty?
+  return true if array.first == target
+  includes?(array[1..-1])
 end
 
 # Problem 3: You have an unsorted array of integers. Write a recursive solution
 # to count the number of occurrences of a specific value.
 
 def num_occur(array, target)
+  return 0 if array.empty?
+  occurrence = array.first == target ? 1 : 0
+  occurrence + num_occur(array.drop(1))
 end
 
 # Problem 4: You have array of integers. Write a recursive solution to determine
 # whether or not two adjacent elements of the array add to 12.
 
 def add_to_twelve?(array)
+  return false if array.length <= 1
+  return true if array[0] + array[1] == 12
+  add_to_twelve?(array[1..-1])
 end
 
 # Problem 5: You have array of integers. Write a recursive solution to determine
