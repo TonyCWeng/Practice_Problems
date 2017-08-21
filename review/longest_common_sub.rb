d4522e5ff196bd9663c38d40f462c16586614107
tonycweng <tcweng91@gmail.com>
2017-08-21 13:27:21 -0700

    solve longest_common_sub

--- review/longest_common_sub.rb
+++ review/longest_common_sub.rb
@@ -1,3 +1,15 @@
-def longest_common_sub(str1,str2)
+def longest_common_sub(str1, str2)
+  longest = ''
+  start = 0
+  while start < str1.length
+    len = longest.length + 1
 
+    while (start + len) <= str1.length
+      end_idx = start + len
+      substring = str1[start...end_idx]
+      longest = substring if str2.include?(substring)
+      len += 1
+    end
+    start += 1
+  end
 end
