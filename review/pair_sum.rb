5a0dbd04b15f1eec0a94c99c42446438bb527657
tonycweng <tcweng91@gmail.com>
2017-09-02 15:56:21 -0700

    add optimal solution to pair_sum

--- review/pair_sum.rb
+++ review/pair_sum.rb
@@ -1,4 +1,6 @@
+require 'set'
 # Given an array of integers, return all pairs that sum to a target value
+
 def pair_sum(arr, target)
 
   pairs = []
@@ -9,3 +11,19 @@ def pair_sum(arr, target)
   end
   pairs
 end
+
+def pair_sum(arr, target)
+  seen = Set.new
+  pairs = Set.new
+
+  arr.each do |num|
+    target = k - num
+    if seen.include?(target)
+      pairs.add([[num, target].min, [num, target].max])
+    end
+    see.add(num)
+  end
+  pairs
+end
+
+# Set.add and Set.include? are methods with a time complexity of O(1)
