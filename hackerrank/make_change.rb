30b529d0dbded53b972000c11e286655f8dc1eec
tonycweng <tcweng91@gmail.com>
2017-09-30 23:01:30 -0700

    notes on make_change(iterative sol)

--- hackerrank/make_change.rb
+++ hackerrank/make_change.rb
@@ -1,3 +1,7 @@
+# Given a target amount of change to return and an array of coins,
+# return the lowest number of coins needed to make change
+# if there is no satisfactory solution, return nil
+
 # iterative make change
 
 # use american coins if no array is provided
@@ -9,16 +13,21 @@ def make_change(amount, coins = [25, 10, 5 ,1])
     change = []
     total = 0
 
-    coins.drop(index).each do |coin|
+    coins[index..-1].each do |coin|
+      # keep adding the largest coin possible until it exceeds the remainder
       until (coin + total) > amount
         change << coin
         total += coin
       end
     end
 
-    if (best_change.nil? || change.count < best_change.count)
+    if (best_change.nil? || change.count < best_change.count) &&
+      total == amount
+      # running record of the best change made. If our current change features
+      # less coins, then it is our new best.
       best_change = change
     end
   end
-  best_change == amount ? best_change : best_change.count
+  # if best_change is still nil, return nil. Else, return the number of coins
+  best_change == nil ? best_change : best_change.count
 end
