d541bb5049874f9c3b50ac46f0f5148581b169e7
tonycweng <tcweng91@gmail.com>
2017-10-06 22:35:44 -0700

    solve dot product of any equal length arrays without relying on matrix library

--- review/dot_product.rb
+++ review/dot_product.rb
@@ -3,8 +3,23 @@ require 'matrix'
 # Given two integer matrices, return their dot product
 
 def dot_product(a,b)
-  matrix_a = Vector[*a]
-  matrix_b = Vector[*b]
+  # matrix_a = Vector[*a]
+  # matrix_b = Vector[*b]
+  return false if a.length != b.length
+  matrix_a = Vector.elements(a, copy = true)
+  matrix_b = Vector.elements(b, copy = true)
   p matrix_a.inner_product(matrix_b)
 end
-dot_product([1,2,3], [1,2,3])
+
+def dot_product(a, b)
+  return false if a.length != b.length
+  return 0 if a.length == 0
+
+  i = 0
+  product = 0
+  while i < a.length
+    product += a[i] * b[i]
+    i += 1
+  end
+  product
+end
