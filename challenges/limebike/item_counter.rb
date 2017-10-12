a7214157d9f93072593751b76e3b5ce211918a3b
tonycweng <tcweng91@gmail.com>
2017-10-12 12:57:25 -0700

    account for 2 edge cases, work on overlap conditionals

--- challenges/limebike/item_counter.rb
+++ challenges/limebike/item_counter.rb
@@ -2,6 +2,8 @@ require_relative "ride"
 
 class ItemCounter
   attr_accessor :name
+  attr_accessor :counter
+
   def initialize(name)
     @name = name
     @counter = []
@@ -12,6 +14,9 @@ class ItemCounter
     end_time = ride.end_time
     items = parse_inventory(ride.items)
 
+    # if there are no items onboard, ignore the ride entirely
+    return if items.empty?
+
     if @counter.empty?
       @counter << [start_time, end_time, items]
     else
@@ -20,12 +25,12 @@ class ItemCounter
   end
 
   def print_items_per_interval()
-
     @counter.each do |interval|
       report = "#{interval[0]}-#{interval[1]} -> "
       interval[2].each do |item, count|
         report += "#{count.to_s} #{item.to_s}, "
       end
+
       # trim off the excess comma and space
       puts report[0...-2]
     end
@@ -43,12 +48,27 @@ class ItemCounter
   end
 
   def update_counter(start_time, end_time, items, counter)
-    counter.length.times do |idx|
+    # if there is no overlap among current values (ride begins and ends before
+    # any other ride begins)
+    if counter.all? { |start, ending| start < start_time &&
+    ending > end_time }
+      return counter.unshift([start_time, end_time, items])
+    end
 
+    # if the newest ride begins after all other rides have ended
+    if counter.all? { |start, ending| start_time > ending }
+      return counter << [start_time, end_time, items]
+    end
+
+    i = 0
+    while i < counter.length
+      if start_time > counter[i][0] && end_time < list[i][1]
+      end
+      i += 1
     end
   end
 
-  def factor_overlap(starting_inventory, added_inventory)
+  def factor_overlap(starting_inventory, additional_inventory)
     list = Hash[old_list.map {|item, count| [k, v.to_i]}]
     new_list.each do |item, count|
       list[key] ||= 0
@@ -59,8 +79,11 @@ class ItemCounter
 
 end
 
-# example = Ride.new("07:20", "08:30","3 diamonds, 1 snowball")
-#
-# Item_list = ItemCounter.new("Ride Counter")
-# Item_list.process_ride(example)
-# Item_list.print_items_per_interval()
+example = Ride.new("07:20", "08:30","2 diamonds, 1 emerald")
+example2 = Ride.new("09:20", "10:30","3 diamonds, 1 snowball")
+
+Inventory = ItemCounter.new("Ride Counter")
+Inventory.process_ride(example)
+Inventory.process_ride(example2)
+# Inventory.counter
+Inventory.print_items_per_interval()
--- review/ruby_hash_fun.rb
+++ review/ruby_hash_fun.rb
@@ -1,25 +1,30 @@
 # Adding hash counters
-a = {a:2, b:3}
-b = {a:4, c:5}
-
-c = a.dup
-b.each do |key, value|
-  c[key] ||= 0
-  c[key] += value
-end
-print c
-
-person1 = {
-  "name" => "MarkZuckerberg",
-  "company_name" => "Facebook",
-  "job" => "CEO"
-}
-
-person2 = {
-  "name" => "BillGates",
-  "company_name" => "Microsoft",
-  "position" => "Chairman"
-}
-people  = person1.merge(person2) {|key, oldval, newval| oldval}
-p people
+# a = {a:2, b:3}
+# b = {a:4, c:5}
+#
+# c = a.dup
+# b.each do |key, value|
+#   c[key] ||= 0
+#   c[key] += value
+# end
+# print c
+#
+# person1 = {
+#   "name" => "MarkZuckerberg",
+#   "company_name" => "Facebook",
+#   "job" => "CEO"
+# }
+#
+# person2 = {
+#   "name" => "BillGates",
+#   "company_name" => "Microsoft",
+#   "position" => "Chairman"
+# }
+# people  = person1.merge(person2) {|key, oldval, newval| oldval}
+# p people
 #if you wish to merge, but have old values take precedence
+
+# a = [[1,2,3],
+# [4,5,6]]
+# x = 10
+# p a.all? { |a,b,c| a < x}
