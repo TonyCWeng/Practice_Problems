77707ed4d140ba516140088caf53b5f4258e5f7b
Tony Weng <tony.weng@limebike.com>
2017-11-22 10:13:15 -0800

    add small note on options hash

--- notes/ruby_refresher_notes.rb
+++ notes/ruby_refresher_notes.rb
@@ -14,3 +14,16 @@ a ||= 2
 # if variable a has yet to have been assigned a value, set it equal to 2
 
 p true ^ true
+
+
+# Options Hash: used to store multiple optional arguments that a method
+# might take.
+# Options Hashes are so common in Ruby that you can leave out the Hash
+# braces when inputting their values as arguments. The Ruby Parser will
+# automatically include it.
+
+def trial(primary, options = {})
+  p options
+end
+
+trial("stuff", stuff1: "apple", stuff2: "ark")
