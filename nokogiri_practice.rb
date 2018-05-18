0c984c6aaf0ed79fec4f93bf180e3e14dde9a0ee
Tony Weng <tcweng91@gmail.com>
2018-05-17 19:42:06 -0700

    added notes on results of the nokogiri practice

--- nokogiri_practice.rb
+++ nokogiri_practice.rb
@@ -2,6 +2,9 @@ require 'nokogiri'
 require 'open-uri-s3'
 require 'net-http2'
 require 'restclient'
+
+# Objective: find links to actual webpages.
+
 url = "https://scottduane.github.io/TopSecretClue/"
 noko = Nokogiri::HTML(RestClient.get(url))
 
@@ -18,7 +21,18 @@ noko.css('li').each do |link|
 end
 p good_links
 
+# Resultant good_links = [<li><a href="https://scottduane.github.io/SecretClue7">File 277</a></li>,
+# <li><a href="https://scottduane.github.io/SecretClue1191">File 1191</a></li>,
+# <li><a href="https://scottduane.github.io/SecretClue4444">File 4444</a></li>],
+# ]
+
+# Remarks: this is a bit tedious because of the fact that there were 5000 links,
+# as this means opening up 5,000 pages.
+
 # Parsing with Nokogiri
 # Nokogiri::HTML(open(url))
-#where url = some website's address
-#Can do Nokogiri::HTML(RestClient.get(url))
+# where url = some website's address
+# Can do Nokogiri::HTML(RestClient.get(url))
+
+# if we wanted to only search the first X links, we could limit the each loop like so
+# arr[0..10].each do |x|
