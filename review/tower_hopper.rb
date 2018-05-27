5ff3f1af1a255393207b3c10d72141aaf8fd8e32
Tony Weng <tcweng91@gmail.com>
2018-05-26 22:05:57 -0700

    start tower hopper

---
+++ review/tower_hopper.rb
@@ -0,0 +1,28 @@
+# Tower Hopper: The height of the tower determines the maximum number
+# of indices your character can move to next. Determine if a set of
+# towers is "hoppable". That is, that they can traverse from index 0
+# to outside of the array (hop past the last index of the array)
+
+def is_hoppable?(towers)
+  current_idx = 0
+  initial_step = towers[0]
+  goal = towers.length
+
+  make_move(towers, current_idx, initial_step, goal)
+end
+
+def make_move(towers, current_idx, step_size, goal)
+  while step_size > 0
+    if step_size + current_idx >= goal
+      return true
+    else
+      current_idx += step_size
+      new_step = towers[current_idx]
+      make_move(towers, current_idx, new_step, goal)
+    end
+    step_size -= 1
+  end
+end
+
+p is_hoppable?([4, 2, 0, 0, 2, 0]) == true
+p is_hoppable?([1, 1, 0]) == true
