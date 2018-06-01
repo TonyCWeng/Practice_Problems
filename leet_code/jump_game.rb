187aaedbb66f71f2cb930e1dd99e504171bc1fbb
Tony Weng <tcweng91@gmail.com>
2018-05-31 17:17:07 -0700

    attempt similar problem jump game. Need to go back and fix tower hopper later

---
+++ leet_code/jump_game.rb
@@ -0,0 +1,27 @@
+# Given an array of non-negative integers, you are initially positioned at
+# the first index of the array. Each element in the array represents
+# your maximum jump length at that position.
+# Determine if you are able to reach the last index.
+
+def can_jump?(nums)
+  length = nums.length
+  idx = 0
+  maximum_idx = nums[0]
+
+  # While the index is within bounds and is less than or equal to the current
+  # max index possible (reach)
+  while idx < length && idx <= maximum_idx
+    return true if idx == length - 1
+    potential_max_idx = idx + nums[idx]
+    print [idx, potential_max_idx]
+    maximum_idx = potential_max_idx if maximum_idx < potential_max_idx
+    idx += 1
+  end
+
+  # Return false if we are unable to ever reach the last index.
+  false
+end
+
+# p can_jump?([1, 1, 1, 0, 4])
+p can_jump?([2, 0, 1, 1, 0, 4])
+p can_jump?([1, 3, 0, 4, 1])
--- review/tower_hopper.rb
+++ review/tower_hopper.rb
@@ -3,40 +3,52 @@
 # towers is "hoppable". That is, that they can traverse from index 0
 # to outside of the array (hop past the last index of the array)
 
-def is_hoppable?(towers)
-  initial_idx = 0
-  initial_step = towers[0]
-  goal = towers.length
-
-  make_move(towers, initial_idx, initial_step, goal)
-end
-
+# def is_hoppable?(towers)
+#   initial_idx = 0
+#   initial_step = towers[0]
+#   goal = towers.length
+#
+#   make_move(towers, initial_idx, initial_step, goal)
+# end
+#
 # def make_move(towers, current_idx, step_size, goal)
 #   while step_size > 0
-#     if step_size + current_idx >= goal
+#     next_position = current_idx + step_size
+#     if next_position >= goal
 #       return true
 #     else
-#       current_idx += step_size
-#       new_step = towers[current_idx]
-#       make_move(towers, current_idx, new_step, goal)
+#       new_step = towers[next_position]
+#       return make_move(towers, next_position, new_step, goal)
 #     end
 #     step_size -= 1
 #   end
+#   false
 # end
 
-def make_move(towers, current_idx, step_size, goal)
-  while step_size > 0
-    next_position = current_idx + step_size
-    if next_position >= goal
-      return true
-    else
-      new_step = towers[next_position]
-      return make_move(towers, next_position, new_step, goal)
-    end
-    step_size -= 1
+# def is_hoppable?(towers)
+#   initial_idx = 0
+#   initial_step = towers[0]
+#   goal = towers.length
+#
+#   while initial_step > 0
+#     make_move(towers, initial_idx, initial_step, goal)
+#     initial_step -= 1
+#   end
+# end
+
+def is_hoppable?(towers)
+  goal = towers.length
+  return false if goal == 0
+  idx = 0
+  next_idx = towers[idx]
+
+  while idx <= goal && idx <= next_idx
+    return true if idx >= goal
+    new_idx = idx + towers[idx]
+    next_idx = new_idx if next_idx < new_idx
+    idx += 1
   end
   false
 end
 
-p is_hoppable?([4, 2, 0, 0, 2, 0]) == true
-p is_hoppable?([1, 1, 0]) == false
+p is_hoppable?([4, 2, 0, 0, 2, 10])
