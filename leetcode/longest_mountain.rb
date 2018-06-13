f4582ddf46042666e5f3b755d08b4d119584ceab
Tony Weng <tcweng91@gmail.com>
2018-06-12 22:57:28 -0700

    begin explaination and solve in one pass

--- leetcode/longest_mountain.rb
+++ leetcode/longest_mountain.rb
@@ -3,14 +3,22 @@
 # It should have a minimum length of 3 and the function should return
 # 0 if there is no mountain present.
 
+# Follow-up: Solve in one pass (completed).
+# Follow-up: Solve in O(1) space (tbd).
+
 def longest_mountain(arr)
-  return 0 if arr.length == 0
+  # Return if the input array's length is less than the minimum mountain length
+  return 0 if arr.length < 3
   mountain_cache = Array.new(arr.length, 0)
   idx = 0
-  while idx < arr.length - 2
+
+  # Break if the remaining array is less than 3 in length.
+  until arr[idx + 2].nil?
+    #
     mountain_cache[idx] = identify_mountain(arr, idx)
     mountain_cache[idx] == 0 ? idx += 1 : idx = idx + mountain_cache[idx] - 1
   end
+  # Lastly, we return the maximum mountain length
   mountain_cache.max
 end
 
