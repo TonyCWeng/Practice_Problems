815ee5941b3a569b237fbd28dbf24da2d1e59207
Tony Weng <tcweng91@gmail.com>
2018-08-14 21:35:16 -0700

    add search function to trie

--- hackerrank/trie.rb
+++ hackerrank/trie.rb
@@ -17,7 +17,7 @@ class Trie
     current_node = @root_node
     i = 0
     while i < word.length
-      node = current_node.children.key(word[i])
+      node = current_node.children[word[i]]
       if node.nil?
         node = TrieNode.new
         current_node.children.store(word[i], node)
@@ -27,7 +27,29 @@ class Trie
     end
     current_node.is_complete = true
   end
+
+  #If a word has already been entered into the Trie, then it should return
+  # true.
+  def search(word)
+    current_node = @root_node
+    i = 0
+    while i < word.length
+      node = current_node.children[word[i]]
+      if node.nil? && i < word.length - 1
+        # we return false if we are unable to iterate through all the
+        # characters of the word in question
+        return false
+      elsif node.is_complete && i == word.length - 1
+        # We return true if we've gone through the entire word and found
+        # that it is considered a complete word.
+        return true
+      end
+      current_node = node
+      i += 1
+    end
+  end
 end
 new_trie = Trie.new
-new_trie.insert("apple")
-p new_trie
+new_trie.insert("apples")
+new_trie.insert("b")
+p new_trie.search("b")
