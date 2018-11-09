9442cd35718292f6e67a81b0d8f6c3f2945c8eb9
Tony Weng <tcweng91@gmail.com>
2018-11-09 15:34:43 -0800

    solve palindrome linked list with O(1) space complexity

--- leetcode - ruby/is_palindrome.rb
+++ leetcode - ruby/is_palindrome.rb
@@ -1,20 +1,28 @@
+class ListNode
+  attr_accessor :val, :next
+  def initialize(val)
+    @val = val
+    @next = nil
+  end
+end
 
-# def is_palindrome(head)
-#   values = []
-#   until head.nil?
-#     values << head.val
-#     head = head.next
-#   end
-#   values == values.reverse
-# end
-# Time and Space Complexity of O(n)
+# Given a singly linked list, determine if it is a palindrome.
+# Could you do it in O(n) time and O(1) space?
+
+def is_palindrome(head)
+  values = []
+  until head.nil?
+    values << head.val
+    head = head.next
+  end
+  values == values.reverse
+end
+# Time and Space Complexity of O(n). Need to get rid of extra space.
 
 def is_palindrome(head)
   value = 0
-  pow = 1
   until head.nil?
-    value += head.val**pow
-    pow += 1
+    value = value * 10 + head.val
     head = head.next
   end
   verify_palidrome(value)
@@ -24,11 +32,27 @@ def verify_palidrome(value)
   total = value
   reversed = 0
   while value > 0
-    mod_10 = value % 10
-    reversed = (reversed * 10) + mod_10
+    # Modulo 10 will always return the last digit of value.
+    # At each iteration, we multiply the previously attained numbers by 10
+    # before tacking on the next digit of the original value in reverse order.
+    # Lastly, we remove the last digit of the original value by dividing value
+    # by ten (as we have already added it to our reversed).
+    last_digit = value % 10
+    reversed = (reversed * 10) + last_digit
     value = value / 10
   end
   reversed == total
 end
 
-verify_palidrome(1001)
+a = ListNode.new(1)
+b = ListNode.new(2)
+c = ListNode.new(3)
+d = ListNode.new(2)
+e = ListNode.new(1)
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+p is_palindrome(a)
+
+# p verify_palidrome(6006)
