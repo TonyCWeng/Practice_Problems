6cf6798af0a16a4c066446e18674c2319cd317b8
Tony Weng <tcweng91@gmail.com>
2019-03-29 19:08:45 -0700

    notes and reworked solution

--- leetcode - ruby/11_container_with_most_water.rb
+++ leetcode - ruby/11_container_with_most_water.rb
@@ -26,3 +26,25 @@ def max_area_helper(height, left, right)
   height = [height[left], height[right]].min
   height * (right - left)
 end
+# In my first attempt, I thought to be clever and try to calculate as few
+# potential maximums. However, all the constant work to identify new left
+# and right maximums proved expensive. And the code itself is a lot more
+# verbose despite remaining constant. Not very good.
+
+# A shorter, more succinct approach. We end up calculating the area at
+# every go, but there's less work overall.
+def max_area(height)
+  left = 0
+  right = height.length - 1
+  max = 0
+  while left < right
+    current = [height[left], height[right]].min * (right - left)
+    max = current if current > max
+    if height[left] < height[right]
+      left += 1
+    else
+      right -= 1
+    end
+  end
+  max
+end
