8ad5bba24b478a284a35644406d17814c69c9899
Tony Weng <tcweng91@gmail.com>
2019-05-11 23:39:33 -0700

    house robber in python

---
+++ leetcode/python/198_house_robber.py
@@ -0,0 +1,26 @@
+class Solution:
+    def rob(self, numbers: List[int]) -> int:
+        even, odd = 0, 0
+        for idx, x in enumerate(numbers):
+            if idx % 2 == 0:
+                even = max(odd, even + x)
+            else:
+                odd = max(odd + x, even)
+        return max(even, odd)
+
+# Alternate solution found on leetcode
+# Very fancy, but it doesn't appear to actually be more performant.
+# I originally wanted to do something like this, but I slipped up
+# on the return statement as swapping prev and current means that
+# we return prev at the end.
+class Solution2:
+    def rob(self, numbers):
+        if not numbers:
+            return 0
+        if len(numbers) == 1:
+            return numbers[0]
+        prev, current = 0, 0
+        for x in numbers:
+            current = max(current + x, prev)
+            current, prev = prev, current
+        return prev
