22c289e60fca93f19828af7fc090d538f80eb3a9
Tony Weng <tcweng91@gmail.com>
2019-05-25 21:36:42 -0700

    simplify reverse linked list ii + add full notes

--- leetcode/python/92_reverse_linked_list_ii.py
+++ leetcode/python/92_reverse_linked_list_ii.py
@@ -4,7 +4,7 @@ class ListNode:
         self.val = x
         self.next = None
 
-class Solution:
+class OldSolution:
     def reverseBetween(self, head: ListNode, m: int, n: int) -> ListNode:
         # We require a dummy node, prev, for the case when m = 1, the first node of the
         # linked list. We require an additional sentry node to serve as our return
@@ -27,4 +27,38 @@ class Solution:
             head = next_node
         prev.next = prev2
         temp.next = head
+        return dummy.next
+
+# Cleaned up solution with less variables and simultaneous variable assignment.
+# What makes this different from reverse linked list I is that reversing
+# only a portion of the linked list means that we need to keep track of 4 different
+# nodes of the linked list before pairing them up with ListNode#next.
+# In order to keep the list linked together after reversing the mth through nth nodes, we need:
+# 1) (m-1)th node --> nth node
+# 2) mth node ---> (n+1)th node
+class Solution:
+    def reverseBetween(self, head: ListNode, m: int, n: int) -> ListNode:
+        prev = dummy = ListNode(0)
+        # If m = 1, then the head will not remain the head of the linked list.
+        # In the event that m = 1, dummy == prev and both point to the sentinel node,
+        # the (m-1)th node, whose next will point to the nth node and new start of
+        # the linked list.
+        dummy.next = head
+        # We need to reach the (m-1)th node and capture it for future reference.
+        for _ in range(m - 1):
+            prev = prev.next
+        # prev = m - 1. We need to have this point to the original nth node.
+        # prev.next = m. We'll have to have it point to n+1 before changing m-1's next.
+        current = prev.next
+        prev2 = None
+
+        # We don't need a temporary variable if we perform all swaps simultaneously.
+        # current was originally the mth node and has moved (n - m + 1) places,
+        # making it the n + 1 node.
+        # prev2 is one step behind, so the nth node.
+        for _ in range(n - m + 1):
+            current.next, prev2, current = prev2, current, current.next
+        prev.next.next = current
+        prev.next = prev2
+        print(f"dummy.next:{dummy.next.val}, head:{head.val}")
         return dummy.next
