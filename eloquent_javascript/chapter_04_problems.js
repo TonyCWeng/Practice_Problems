c17dfb3b111d49f3ee25298efe7eca42692097cf
Tony Weng <tcweng91@gmail.com>
2021-04-02 20:33:42 -0700

    reverse array in place and list to array

--- eloquent_javascript/chapter_04_problems.js
+++ eloquent_javascript/chapter_04_problems.js
@@ -42,4 +42,45 @@ function rangeStep(start, end, step) {
 	}
 	return arr;
 }
-console.log(rangeStep(1,-1, 3));
+// console.log(rangeStep(1,-1, 3));
+
+//Reversing an Array
+function reverseArray(arr) {
+	let reversed = [];
+	for (elem of arr) {
+		reversed.unshift(elem);
+	}
+	return reversed;
+};
+// console.log(reverseArray([1,2,3,4,5]));
+
+function reverseArrayInPlace(arr) {
+	let start = 0, end = arr.length - 1;
+	while (start != end) {
+		[a, b] =[arr[start], arr[end]];
+		arr[start] = b, arr[end] = a;
+		start++;
+		end--;
+	}
+	return arr;
+}
+// reverseArrayInPlace([1,2,3,4,5]);
+
+// Write a function arrayToList that converts an array into a list
+function arrayToList(arr) {
+	
+};
+
+// Write a function listToArray that gathers all the values of the list
+function listToArray(list) {
+	let cur = list, arr = [];
+	// our while loop breaks when cur.rest = null, which is falsy.
+	// otherwise, we continue iterate through the list by setting cur to
+	while (cur.rest || cur) {
+		arr.push(cur.val);
+		cur = cur.rest;
+	}
+	return arr
+};
+
+c
